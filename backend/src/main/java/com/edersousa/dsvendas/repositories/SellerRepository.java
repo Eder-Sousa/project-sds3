b2ef55169e6d9b8a50416d12cd0803292ed4aa30
Eder-Sousa <ederdi395@gmail.com>
2021-05-07 22:34:14 -0300

    Layers

---
+++ backend/src/main/java/com/edersousa/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.edersousa.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.edersousa.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
